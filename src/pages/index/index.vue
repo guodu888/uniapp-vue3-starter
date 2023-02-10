93888192aff369633c77d60638e2d1b13b618b94
kingzhang1228 <zhang_xxyy66@163.com>
2023-02-10 09:40:02 +0800

    Feat：添加测试页面

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,16 +1,34 @@
+<!--
+ * @Author: Arthur_Zhang
+ * @Date: 2023-02-09 14:51:27
+ * @LastEditors: Arthur_Zhang
+ * @LastEditTime: 2023-02-10 09:38:39
+ * @Description:
+-->
 <script setup lang="ts">
+const data = ref()
+
 function handleToLogin() {
   uni.navigateTo({
     url: '/pages/login/index',
+  })
+}
+function handleToTest() {
+  uni.navigateTo({
+    url: '/pages/test/index',
   })
 }
 </script>
 
 <template>
   <view class="p-3">
+    {{ data }}
     Index
     <button plain @tap="handleToLogin">
       To Login
+    </button>
+    <button class="mt-20rpx" plain @tap="handleToTest">
+      To test
     </button>
   </view>
 </template>
---
+++ src/pages/test/index.vue
@@ -0,0 +1,50 @@
+<!--
+ * @Author: Arthur_Zhang
+ * @Date: 2023-02-10 09:36:24
+ * @LastEditors: Arthur_Zhang
+ * @LastEditTime: 2023-02-10 09:36:42
+ * @Description:
+-->
+<script setup lang='ts'>
+const showStudentBottomPop = ref(false)
+function handleShowBottomPop() {
+  showStudentBottomPop.value = !showStudentBottomPop.value
+}
+function handleCloseBottomPop() {
+  showStudentBottomPop.value = false
+}
+const showStudentLeftPop = ref(false)
+function handleShowLeftPop() {
+  showStudentLeftPop.value = !showStudentBottomPop.value
+}
+function handleCloseLeftPop() {
+  showStudentLeftPop.value = false
+}
+</script>
+
+<template>
+  <div>
+    <button @tap="handleShowBottomPop">
+      bottom pop
+    </button>
+    <guodu-drawer
+      :visible="showStudentBottomPop"
+      :mask-click="true"
+      mode="bottom"
+      @close="handleCloseBottomPop"
+    >
+      bottom pop
+    </guodu-drawer>
+    <button @tap="handleShowLeftPop">
+      bottom left
+    </button>
+    <guodu-drawer
+      :visible="showStudentLeftPop"
+      :mask-click="true"
+      mode="left"
+      @close="handleCloseLeftPop"
+    >
+      bottom left
+    </guodu-drawer>
+  </div>
+</template>
