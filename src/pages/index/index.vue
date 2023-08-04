562a717fa1e3738a0dc76e3175ef79885e82acde
shy1118 <shy1118999@qq.com>
2023-08-04 16:37:14 +0800

    feat: logout

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -4,12 +4,24 @@ function to(path: string) {
     url: path,
   })
 }
+const userStore = useUserStore()
+function handleLogout() {
+  userStore.logout().then(() => {
+    // 重定向到登录页面
+    uni.redirectTo({
+      url: '/pages/login/index',
+    })
+  })
+}
 </script>
 
 <template>
   <view class="p-3">
     <button @tap="to('/pages/login/index')">
       To Login
+    </button>
+    <button @tap="handleLogout">
+      Logout
     </button>
     <button @tap="to('/pages/icon/index')">
       To Icon
