5157f52378e540e9296c6fad789fb0f2ea2f2d75
shy1118 <shy1118999@qq.com>
2023-11-21 10:39:56 +0800

    chore: update deps

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,7 @@ const unocss = require('@unocss/eslint-plugin')
 
 module.exports = antfu(
   {
-    ignores: ['**/manifest.json', '**/pages.json'],
+    ignores: ['**/manifest.json', '**/pages.json', 'src/widgets/guodu-echarts/guodu-echarts.vue'],
   },
   unocss.configs.flat,
 )
